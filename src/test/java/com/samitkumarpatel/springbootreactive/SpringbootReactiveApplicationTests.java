795e21e512e6084b7fefc41c54245ea13a8d394e
Samit Kumar Patel <samitkumarpatel@users.noreply.github.com>
2022-07-31 23:17:54 +0200

    Feature/hem (#1)
    * added helm chart
    
    * added some test
    
    * Disabled a test which need to be invastigate
    
    * helm build added
    
    skip the docker push part if the build are from othan than the default branch

--- src/test/java/com/samitkumarpatel/springbootreactive/SpringbootReactiveApplicationTests.java
+++ src/test/java/com/samitkumarpatel/springbootreactive/SpringbootReactiveApplicationTests.java
@@ -1,13 +1,15 @@
 package com.samitkumarpatel.springbootreactive;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
@@ -61,5 +63,44 @@ class HandlerTest {
 			assertEquals(200, serverResponse.rawStatusCode());
 		}).verifyComplete();
 
+	}
+}
+@SpringBootTest
+class RouterTest {
+	private WebTestClient webTestClient;
+	@Autowired
+	private ApplicationContext context;
+
+	@BeforeEach
+	void setUp() {
+		webTestClient = WebTestClient.bindToApplicationContext(context).build();
+	}
+
+	@Test
+	@DisplayName("route /one test")
+	void routeOneTest() {
+		webTestClient
+				.get()
+				.uri("/one")
+				.header("Content-Type", "application/json")
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody(String.class).value(text -> assertEquals("ONE", text));
+	}
+
+	@Test
+	@DisplayName("route /message test")
+	@Disabled
+	void routeMessageTest() {
+		webTestClient
+				.post()
+				.uri(uriBuilder -> uriBuilder.path("/message").queryParam("messageTo", 1l).build())
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)
+				.body(Mono.just(Message.builder().id(1l).message("your account is activated").build()),Message.class)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(String.class).value(text -> assertEquals("SUCCESS",text));
 	}
 }
