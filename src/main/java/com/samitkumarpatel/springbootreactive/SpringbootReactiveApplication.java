3770a6244d4d084c920671dcd445dce51d9f4b97
samitkumarpatel <samitkumarpatel@gmail.com>
2022-08-05 00:21:00 +0200

    error handling on the loop

--- src/main/java/com/samitkumarpatel/springbootreactive/SpringbootReactiveApplication.java
+++ src/main/java/com/samitkumarpatel/springbootreactive/SpringbootReactiveApplication.java
@@ -1,7 +1,10 @@
 package com.samitkumarpatel.springbootreactive;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.reactive.function.server.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -17,6 +21,7 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 import java.util.UUID;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.*;
 import static org.springframework.http.MediaType.*;
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
@@ -51,7 +56,8 @@ class Router {
 	public RouterFunction route(Handler handler) {
 		return RouterFunctions
 				.route(GET("/one").and(contentType(APPLICATION_JSON)),handler::one)
-				.andRoute(POST("/message").and(contentType(APPLICATION_JSON)), handler::sentMessage);
+				.andRoute(POST("/message").and(contentType(APPLICATION_JSON)), handler::sentMessage)
+				.andRoute(POST("/iterate").and(contentType(APPLICATION_JSON)),handler::iterate);
 	}
 }
 
@@ -85,8 +91,80 @@ class Handler {
 				})
 		,String.class);
 	}
+
+	public Mono<ServerResponse> iterate(ServerRequest request) {
+		return request.bodyToMono(IModel.class)
+				.flatMap(iModel -> {
+					return Flux.fromIterable(iModel.getInts())
+							.doOnNext(i -> log.info("PROCESSING {} ",i))
+							.flatMap(i -> calculate(i)
+									//This one is equivalent to .onErrorContinue but the position matter. For more Info , Please take a look: https://devdojo.com/ketonemaniac/reactor-onerrorcontinue-vs-onerrorresume
+									.onErrorResume(err -> {
+										log.info("onErrorResume");
+										return Mono.just(0);
+									})
+							)
+							/*
+							.onErrorContinue((err,i) -> {
+								log.error("err: {}, i: {}",err.getMessage(),i);
+								//This will not return anything but continue the flow
+							})*/
+							.flatMap(calculatedI -> mapToIData(calculatedI,iModel.getName()))
+
+							.collectList()
+							.flatMap(listOfIData -> {
+								return ServerResponse.ok().body(
+										Mono.just(IDomain.builder().iData(listOfIData).build())
+										,IDomain.class);
+							});
+				});
+
+	}
+
+	private Mono<IDomain.IData> mapToIData(Integer calculatedI, String name) {
+		return Mono.just(
+				IDomain.IData.builder().name(name).value(calculatedI!=0 ? calculatedI : null).result(calculatedI!=0 ? Result.PASS: Result.FAIL).build()
+		);
+	}
+
+	private Mono<Integer> calculate(Integer i) {
+		return Mono.fromCallable(() -> calculator(i,5))
+				.flatMap(val -> {
+					return Mono.just(val);
+				})
+				.doOnSuccess(val -> log.info("SERVICE CALL SUCCESS FOR {} WITH RESULT {}",i,val))
+				.doOnError(e -> log.error("SERVICE CALL FAILURE FOR {} WITH ERROR {}",i,e.getMessage()))
+				.retry(3);
+	}
+
+	//assume It's a legacy service call
+	private Integer calculator(Integer i, Integer multiplyWithNumber) {
+		if(i == Integer.valueOf(3)) {
+			throw new RuntimeException("Number 3 is unlucky number");
+		}
+		return i * multiplyWithNumber;
+	}
+}
+@Data @Builder @AllArgsConstructor @RequiredArgsConstructor
+class IModel {
+	private String name;
+	private List<Integer> ints;
 }
 
+@Data @Builder @AllArgsConstructor @RequiredArgsConstructor
+class IDomain {
+	private List<IData> iData;
+	@Data @Builder @AllArgsConstructor @RequiredArgsConstructor
+	public static class IData {
+		private String name;
+		private Integer value;
+		private Result result;
+	}
+}
+
+enum Result {
+	PASS, FAIL
+}
 @Service
 @Slf4j
 class UserService {
